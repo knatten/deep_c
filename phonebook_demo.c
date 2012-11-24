c338681d0a4eecf5d341c26ab4205a23e216bf40
Anders Schau Knatten <anders@knatten.org>
2012-11-24 12:46:55 +0100

    Improved demo

--- phonebook_demo.c
+++ phonebook_demo.c
@@ -5,29 +5,30 @@
 int main(void)
 {
 	phonebook book = create_phonebook();
-	add(&book, 922, "Anders");
 
+	add(&book, 922, "Anders");
 	const char* name = getByNumber(&book, 922);
 	printf("The owner of number 922 is %s\n",  name);
 
-	number_t number = getByName(&book, "Anders");
-	printf("Anders' number is %d\n", number);
-
-	const char* errorName = getByNumber(&book, 1337);
-	if (errorName == NULL)
-	{
-		printf("No one has number 1337\n");
-	}
-
-	number_t errorNumber = getByName(&book, "Charlotte");
-	if (errorNumber == -1)
-	{
-		printf("Charlotte is not in the phonebook\n");
-	}
-
 	add(&book, 977, "Morten");
+	number_t number = getByName(&book, "Morten");
+	printf("Morten' number is %d\n", number);
 
 	printf("The contents of the phonebook are:\n");
 	print(&book);
+
+	deleteByNumber(&book, 977);
+	const char* errorName = getByNumber(&book, 977);
+	if (errorName == NULL)
+	{
+		printf("No one has number 977\n");
+	}
+
+	deleteByName(&book, "Anders");
+	number_t errorNumber = getByName(&book, "Anders");
+	if (errorNumber == -1)
+	{
+		printf("Anders is not in the phonebook\n");
+	}
 
 }
